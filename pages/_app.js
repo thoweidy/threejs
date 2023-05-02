fa5b5dc93e8878f06dc2a23beeed982be81ae9d0
thoweidy <tamer.howeidy@thecodepro.io>
2023-05-02 16:40:59 -0700

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '@envs/styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ pages/fortress.js
@@ -0,0 +1,34 @@
+import React, { Fragment } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Environment, OrbitControls } from "@react-three/drei";
+
+export default function fortress() {
+  return (
+    <div className='flex items-center justify-center w-screen h-screen p-10'>
+      <Canvas className='bg-black border cursor-grab active:cursor-grabbing'>
+        <OrbitControls />
+        <MyScene />
+
+        <ambientLight />
+        <spotLight intensity={1} angle={0.2} penumbra={1} position={[5, 15, 10]} />
+      </Canvas>
+    </div>
+  );
+}
+
+
+function MyScene() {
+
+  const gltf = useLoader(GLTFLoader, '/green.gltf');
+  console.log(gltf);
+
+  return (
+
+    <Fragment>
+      <primitive object={gltf.scene} scale={0.4} />
+    </Fragment>
+
+  )
+}
